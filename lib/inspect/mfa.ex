e5f4e2a7b6b775500f3a8c9389e6ca3d25980545
Yurii Rashkovskii <yrashk@gmail.com>
2013-09-07 19:00:52 -0700

    Fix ExLogger.Inspect for ExLogger.MFA

--- lib/inspect/mfa.ex
+++ lib/inspect/mfa.ex
@@ -4,13 +4,13 @@ defimpl ExLogger.Inspect, for: ExLogger.MFA do
   import Kernel, except: [to_string: 1]
   
   def to_string(MFA[module: m, function: f, arguments: a, properties: properties]) when is_list(a) do
-    "#{inspect m}.#{inspect f}" <>
+    "#{inspect m}.#{f}" <>
     (Inspect.Algebra.surround_many("(", a, ")", :infinity, inspect(&1)) |> Inspect.Algebra.pretty(:infinity)) <>
     format_properties(properties)
   end
 
   def to_string(MFA[module: m, function: f, arguments: a, properties: properties]) when is_integer(a) do
-    "#{inspect m}.#{inspect f}/#{a}" <> format_properties(properties)
+    "#{inspect m}.#{f}/#{a}" <> format_properties(properties)
   end
 
   defp format_properties(nil), do: ""
